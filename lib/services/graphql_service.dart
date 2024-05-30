2c4a6703073dc6089305c209bc00c5a2b40c3097
jorgeaveraf <jorgeaveraf@gmail.com>
2024-05-29 19:31:46 -0600

    pointing to vm database

--- lib/services/graphql_service.dart
+++ lib/services/graphql_service.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class GraphQLService {
-  static final HttpLink _httpLink = HttpLink('http://10.0.2.2:8000/graphql/');
+  static final HttpLink _httpLink = HttpLink('http://35.247.110.26:8080/graphql/');
 
 
   static final ValueNotifier<GraphQLClient> clientNotifier =
